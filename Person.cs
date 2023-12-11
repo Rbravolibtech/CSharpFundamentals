0ade45f2e24b39bc145e516f6b9c49d5e5587474
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 11:04:50 -0700

    dealing with C# classes

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+	public class Person
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace CSharpFundamentals;
+﻿using System;  // Import the System namespace
+using CSharpFundamentals.Math;
 
-class Program
+namespace CSharpFundamentals
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Cena";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();  
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
     }
 }
-
